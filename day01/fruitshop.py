d4a7a36b5379e33654936571cc7bd2364781ea85
“Cxl” <2229157983@qq.com>
2021-01-05 17:28:47 +0800

    “第一次提交day01”

---
+++ day01/fruitshop.py
@@ -0,0 +1,121 @@
+'''print("——————————————————————————欢迎来到晓玲的水果商城—————————————————————————————————————————")
+print("====================================================================================")
+print("水果名称                  水果价格              水果品质              水果数量")
+print("------------------------------------------------------------------------------------")
+print("苹果                      0.5                    A                  150")
+print("柿子                      0.8                    A                  300")
+print("香蕉                      0.5                    A                  5000")
+print("荔枝                      0.3                    B                  2500")
+print("柚子                      6.6                    A                  3600")
+print("草莓                      3.5                    A+                 300")
+print("砂糖桔                     0.3                   A+                  6000")
+print("车厘子                     3.68                   A+                  6000")
+print("火龙果                     3.3                   B                   600")
+print("菇娘果                     0.3                   A                   7000")
+print("水蜜桃                     0.8                   A+                  6000")
+print("哈密瓜                     9.8                    A                   1000")
+print("无花果                     2.8                    A                   8000")
+print("巨峰葡萄                    0.1                   A                   10000")
+print("无籽西瓜                    26.8                  A+                  5000")
+print("南国酥梨                     6.8                  A+                  3000")
+print("水晶葡萄                    0.8                   A                   5000")
+print("------------------------------------------------------------------------------------")
+print("总金额为：","￥",(0.5*150+0.8*300+0.5*5000+0.3*2500+6.6*3600+3.5*300+0.3*6000+3.68*6000+3.3*600+0.3*70000.8*6000+9.8*1000+2.8*8000+0.1*10000+26.8*5000+6.8*3000+0.8*5000))
+'''
+name16 = "苹果"
+price16 = "0.5"
+quality16 = "A"
+num16 = "150"
+name15="柿子"
+price15="0.8"
+quality15="A"
+num15="300"
+name14="香蕉"
+price14="0.5"
+quality14="A"
+num14="5000"
+name13="荔枝"
+price13="0.3"
+quality13="B"
+num13="2500"
+name12="柚子"
+price12="6.6"
+quality12="A"
+num12="3600"
+name11="草莓"
+price11="3.5"
+quality11="A+"
+num11="300"
+name10="砂糖桔"
+price10="0.3"
+quality10="A+"
+num10="6000"
+name9="车厘子"
+price9="3.68"
+quality9="A+"
+num9="6000"
+name8="火龙果"
+price8="3.3"
+quality8="B"
+num8="600"
+name7="菇娘果"
+price7="0.3"
+quality7="A"
+num7="7000"
+name6="水蜜桃"
+price6="0.8"
+quality6="A+"
+num6="6000"
+name5="哈密瓜"
+price5="9.8"
+quality5="A"
+num5="1000"
+name4="无花果"
+price4="2.8"
+quality4="A"
+num4="10000"
+name3="巨峰葡萄"
+price3="0.1"
+quality3="A"
+num3="10000"
+name2 = "无籽西瓜"
+price2 = "26.8"
+quality2 = "A+"
+num2= "5000"
+name1="南国酥梨"
+price1="6.8"
+quality1="A+"
+num1="3000"
+name="水晶葡萄"
+price="0.8"
+quality="A"
+num="5000"
+print("-------------------------欢迎来到水果商城-------------------------")
+print("水果名称           价格             品质               数量")
+print("-----------------------------------------------------------------")
+info='''
+{name}            {price}                 {quality}                 {num}粒/个
+'''
+
+print(info.format(name=name,price=price,quality=quality,num=num))
+print(info.format(name=name1,price=price1,quality=quality1,num=num1))
+print(info.format(name=name2,price=price2,quality=quality2,num=num2))
+print(info.format(name=name3,price=price3,quality=quality3,num=num3))
+print(info.format(name=name4,price=price4,quality=quality4,num=num4))
+print(info.format(name=name5,price=price5,quality=quality5,num=num5))
+print(info.format(name=name6,price=price6,quality=quality6,num=num6))
+print(info.format(name=name7,price=price7,quality=quality7,num=num7))
+print(info.format(name=name8,price=price8,quality=quality8,num=num8))
+print(info.format(name=name9,price=price9,quality=quality9,num=num9))
+print(info.format(name=name10,price=price10,quality=quality10,num=num10))
+print(info.format(name=name11,price=price11,quality=quality11,num=num11))
+print(info.format(name=name12,price=price12,quality=quality12,num=num12))
+print(info.format(name=name13,price=price13,quality=quality13,num=num13))
+print(info.format(name=name14,price=price14,quality=quality14,num=num14))
+print(info.format(name=name15,price=price15,quality=quality15,num=num15))
+print(info.format(name=name16,price=price16,quality=quality16,num=num16))
+print("-------------------------------------------------------------------")
+
+
+
+
